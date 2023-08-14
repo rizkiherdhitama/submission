2cce0fa4a12de7c641d77a1c0415dbdfec533dc0
rizkiherdhitama <rizkiherdhitama@gmail.com>
2023-08-14 16:33:45 +0700

    ststus model, factory and seeder

---
+++ database/factories/StatusFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use app\Models\Status;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Status>
+ */
+class StatusFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return fake()->randomElement([
+            Status::PENDING,Status::APPROVED, Status::REJECTED
+        ]);
+    }
+
+    public function pending(): static
+    {
+        return $this->state(fn(array $attributes) => Status::PENDING);
+    }
+    public function approved(): static
+    {
+        return $this->state(fn(array $attributes) => Status::APPROVED);
+    }
+    public function rejected(): static
+    {
+        return $this->state(fn(array $attributes) => Status::REJECTED);
+    }
+}
