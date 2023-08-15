42a7f359ef035becf8e0d884e652d5dd7dcf4326
rizkiherdhitama <rizkiherdhitama@gmail.com>
2023-08-15 11:34:05 +0700

    config timezone and app name

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
@@ -13,4 +14,14 @@ class Status extends Model
     public const PENDING = ['id' => 1, 'name' => 'Pending'];
     public const APPROVED = ['id' => 2, 'name' => 'Approved'];
     public const REJECTED = ['id' => 3, 'name' => 'Rejected'];
+
+    public function submissions(): HasMany
+    {
+      return $this->hasMany(Submission::class);
+    }
+
+    public function histories(): HasMany
+    {
+      return $this->hasMany(History::class);
+    }
 }
