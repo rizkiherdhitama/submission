a438752da52cb7c953ffcddac25ba6d6adfa18f9
rizkiherdhitama <rizkiherdhitama@gmail.com>
2023-08-16 11:03:23 +0700

    Validation, guest

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+  public function create()
+  {
+    return view('session.create');
+  }
+
+  public function store()
+  {
+    $attributes = request()->validate([
+      'email' => 'required|email',
+      'password' => 'required',
+    ]);
+
+    if (auth()->attempt($attributes)) {
+      return redirect('/home')->with('success', 'Welcome back');
+    }
+
+    /* Same result
+    return back()
+      ->withInput()
+      ->withErrors(['email' => 'Your credentials could not be verified']);
+    */
+
+    throw ValidationException::withMessages(
+      ['email' => 'Your credentials could not be verified.']
+    );
+  }
+
+  public function destroy()
+  {
+    $loggedout = auth()->user()->name;
+    auth()->logout();
+
+    return redirect('/')->with('success', "See you again {$loggedout}");
+  }
+}
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -22,7 +22,7 @@
                 <div class="mb-4">
                     <label for="email" class="block text-gray-700 text-sm font-medium mb-1">Email</label>
                     <input type="email" id="email" name="email" value="{{ old('email') }}"
-                        class="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-indigo-200" required>
+                        class="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-indigo-200">
                     @error('email')
                         <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                     @enderror
